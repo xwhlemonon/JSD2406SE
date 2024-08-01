a4d062bd27b34b0b86f81ea07d5c5af6202738de
熊文浩 <dr.xiong.wen.hao@gmail.com>
2024-08-01 11:40:18 +0800

    New Code.

--- src/main/java/DataDemo/JF.java
+++ src/main/java/DataDemo/JF.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class JF extends JFrame {
     public JF() {
-        add(new PicFrame());
+        JPanel jp = new JPanel();
+        add(jp);
+        jp.add(new PicFrame());
     }
 }
